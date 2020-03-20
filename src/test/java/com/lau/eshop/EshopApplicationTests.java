03a3b894a5230fee9186478628c3215a673ed9f6
lau <289899063.com>
2020-03-20 18:56:12 +0800

    Initial commit

---
+++ src/test/java/com/lau/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lau.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
